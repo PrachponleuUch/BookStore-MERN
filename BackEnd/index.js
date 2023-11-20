697bdf70244dce31e7a445233176e1da3b582ba1
PrachponleuUch <ponleuvuthy@gmail.com>
2023-11-20 18:11:05 +1100

    Feature: Implemented the Home Page

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -10,9 +10,9 @@ const app = express();
 //Middleware for parsing request body
 app.use(express.json());
 app.use(cors({
-  origin: '',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type']
+  // origin: '',
+  // methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  // allowedHeaders: ['Content-Type']
 }));
 
 app.listen(PORT, () => {
